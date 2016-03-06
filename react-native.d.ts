9d272402fa4a6b6412485d03b0498b730445fe73
ayoubdev <ayoub.development@gmail.com>
2016-03-06 21:35:07 +0100

    => Updated react-native.d.ts => Added package.json => Added test folder with sample and typescript config

--- react-native.d.ts
+++ react-native.d.ts
@@ -1,157 +1,185 @@
-//REACT.JS ambient import:
-//import * as name from module: permet d'importer tout le contenu d'un module
-//(variables, fonctions...). Le contenu sera accessible via name.xxxxx:
+//Import de tout le contenu du module "react" (on aurait très bien pu passer par une référence vers le fichier react.d.ts
+//afin d'utiliser directement le contenu du module sans import mais contrairement à tsd où les dépendances sont gérés via <reference>
+//dans typings, la génération des dépendances se fait via des imports):
+//ReactJS Api:
 import * as React from "react";
 
-//REACT-NATIVE definitions:
+//On utilise le même nom de namespace que le principal défini dans le module "react"
+//afin d'obtenir une définition globale mixée entre celles de react et celles de react-native
+//lorsque react-native.d.ts sera spécifié en référence dans le point d'entrée principal des définition
+//(i.e. main.d.ts). En effet, le contenu des namespaces avec un même nom sont mixés dans le scope global
+//lors de la psécification de leur fichier via <reference path.../>:
+export namespace __React {
+	//Custom Types:
+	type color = string;
 
-//Custom Types:
-export type color = string;
-
-//Event:
-//Les interfaces ne sont sont pas pris en compte lors de la transpilation:
-//donc si on a dans notre ts: import TestEvent = __React.TestEvent;
-//cette ligne sera ignorée lors de la transpilation: c'est top pour définir des types
-//sans avoir à se soucier d'un import n'existant pas en js.
-//Par contre, les classes sont prises en compte lors de la transpilation:
-export interface NativeEvent {
-	bubbles: boolean;
-	cancelable: boolean;
-	currentTarget: EventTarget;
-	defaultPrevented: boolean;
-	eventPhase: number;
-	isTrusted: boolean;
-	preventDefault(): void;
-	stopPropagation(): void;
-	target: EventTarget;
-	timeStamp: Date;
-	type: string;
-}
-export interface EventLayout extends NativeEvent {
-	nativeEvent: {
-		layout: {
-			x: number;
-			y: number;
-			width: number;
-			height: number;
+	//Event:
+	//Les interfaces ne sont sont pas pris en compte lors de la transpilation:
+	//donc si on a dans notre ts: import TestEvent = __React.TestEvent;
+	//cette ligne sera ignorée lors de la transpilation: c'est top pour définir des types
+	//sans avoir à se soucier d'un import n'existant pas en js.
+	//Par contre, les classes sont prises en compte lors de la transpilation:
+	interface NativeEvent {
+		bubbles: boolean;
+		cancelable: boolean;
+		currentTarget: EventTarget;
+		defaultPrevented: boolean;
+		eventPhase: number;
+		isTrusted: boolean;
+		preventDefault(): void;
+		stopPropagation(): void;
+		target: EventTarget;
+		timeStamp: Date;
+		type: string;
+	}
+	interface EventLayout extends NativeEvent {
+		nativeEvent: {
+			layout: {
+				x: number;
+				y: number;
+				width: number;
+				height: number;
+			}
 		}
 	}
-}
-export interface NativeEventHandler<T extends NativeEvent> {
-	(event:T): void;
+	interface NativeEventHandler<T extends NativeEvent> {
+		(event:T): void;
+	}
+
+	interface FlexboxStyles {
+		alignItems?:"flex-start"|"flex-end"|"center"|"stretch";
+		alignSelf?:"auto"|"flex-start"|"flex-end"|"center"|"stretch";
+		borderBottomWidth?:number;
+		borderLeftWidth?:number;
+		borderRightWidth?:number;
+		borderTopWidth?:number;
+		borderWidth?:number;
+		bottom?:number;
+		flex?:number;
+		flexDirection?:"row"|"column";
+		flexWrap?:"wrap"|"nowrap";
+		height?:number;
+		justifyContent?:"flex-start"|"flex-end"|"center"|"space-between"|"space-around";//string
+		left?:number;
+		margin?:number;
+		marginBottom?:number;
+		marginHorizontal?:number;
+		marginLeft?:number;
+		marginRight?:number;
+		marginTop?:number;
+		marginVertical?:number;
+		padding?:number;
+		paddingBottom?:number;
+		paddingHorizontal?:number;
+		paddingLeft?:number;
+		paddingRight?:number;
+		paddingTop?:number;
+		paddingVertical?:number;
+		position?:"absolute"|"relative";
+		right?:number;
+		top?:number;
+		width?:number;
+	}
+	interface TransformsStyles {
+		transform?:Array<{[key: string]:any}>;
+		transformMatrix?:(props:Object, propName:string, componentName:string) => Error|any;
+	}
+	interface ShadowPropTypesIOS {
+		shadowColor: color;
+		shadowOffset: {
+			width: number;
+			height: number;
+		};
+		shadowOpacity: number;
+		shadowRadius: number;
+	}
+
+	interface ViewStylesAndroid {
+		elevation?:number;
+	}
+	interface ViewStylesIOS {
+	}
+	interface ViewStyles extends FlexboxStyles, TransformsStyles, ViewStylesAndroid, ViewStylesIOS {
+		backfaceVisibility?:"visible"|"hidden";
+		backgroundColor?:color;
+		borderBottomColor?:color;
+		borderBottomLeftRadius?:number;
+		borderBottomRightRadius?:number;
+		borderBottomWidth?:number;
+		borderColor?:color;
+		borderLeftColor?:color;
+		borderLeftWidth?:number;
+		borderRadius?:number;
+		borderRightColor?:color;
+		borderRightWidth?:number;
+		borderStyle?:"solid"|"dotted"|"dashed";
+		borderTopColor?:color;
+		borderTopLeftRadius?:number;
+		borderTopRightRadius?:number;
+		borderTopWidth?:number;
+		borderWidth?:number;
+		opacity?:number;
+		overflow?:"visible"|"hidden";
+	}
+
+	interface TextStyles {
+		//tt:string|number;
+	}
+
+	/*
+	//AllStyles utile si l'on veut restreindre les types et attributs possibles à injecter dans class StyleSheet:
+	//Dans ce cas, on aurait définit:
+	interface AllStyles extends ViewStyles, TextStyles {
+	}
+	class StyleSheet {
+		static create(obj:{[key: string]:AllStyles}):any;
+		static hairlineWidth:number;
+	}
+	*/
+
+	//PROPERTIES:
+	//Définition propriétés <View/>:
+	interface ViewPropsAndroid {
+
+	}
+	interface ViewProps {
+		accessibilityLabel?:string;
+		accessible?:boolean;
+		onAccessibilityTap?:() => void;
+		onLayout?:NativeEventHandler<EventLayout>;
+		onMagicTap?: () => void;
+		//cf. https://facebook.github.io/react-native/docs/gesture-responder-system.html:
+		onMoveShouldSetResponder?: () => void;
+		onMoveShouldSetResponderCapture?: () => void;
+
+		style?:ViewStyles;
+	}
+
+	//COMPONENTS:
+	class Text extends React.Component<any, any> {
+	}
+	class View extends React.Component<ViewProps, any> {
+	}
+	class StyleSheet {
+		static create(obj:{[key: string]:any}):any;
+
+		static hairlineWidth:number;
+	}
+
+	/*
+	//class View extends Component<ViewProps, any> équivalent à:
+	class View extends Component<any, any> {
+		props: {
+			testprop:string
+		}
+		render():JSX.Element;
+	}
+	*/
 }
 
-export interface FlexboxStyles {
-	alignItems?:"flex-start"|"flex-end"|"center"|"stretch";
-	alignSelf?:"auto"|"flex-start"|"flex-end"|"center"|"stretch";
-	borderBottomWidth?:number;
-	borderLeftWidth?:number;
-	borderRightWidth?:number;
-	borderTopWidth?:number;
-	borderWidth?:number;
-	bottom?:number;
-	flex?:number;
-	flexDirection?:"row"|"column";
-	flexWrap?:"wrap"|"nowrap";
-	height?:number;
-	justifyContent?:"flex-start"|"flex-end"|"center"|"space-between"|"space-around";//string
-	left?:number;
-	margin?:number;
-	marginBottom?:number;
-	marginHorizontal?:number;
-	marginLeft?:number;
-	marginRight?:number;
-	marginTop?:number;
-	marginVertical?:number;
-	padding?:number;
-	paddingBottom?:number;
-	paddingHorizontal?:number;
-	paddingLeft?:number;
-	paddingRight?:number;
-	paddingTop?:number;
-	paddingVertical?:number;
-	position?:"absolute"|"relative";
-	right?:number;
-	top?:number;
-	width?:number;
-}
-export interface TransformsStyles {
-	transform?:Array<{[key: string]:any}>;
-	transformMatrix?:(props:Object, propName:string, componentName:string) => Error|any;
-}
-export interface ShadowPropTypesIOS {
-	shadowColor: color;
-	shadowOffset: {
-		width: number;
-		height: number;
-	};
-	shadowOpacity: number;
-	shadowRadius: number;
-}
-
-export interface ViewStylesAndroid {
-	elevation?:number;
-}
-export interface ViewStylesIOS {
-}
-export interface ViewStyles extends FlexboxStyles, TransformsStyles, ViewStylesAndroid, ViewStylesIOS {
-	backfaceVisibility?:"visible"|"hidden";
-	backgroundColor?:color;
-	borderBottomColor?:color;
-	borderBottomLeftRadius?:number;
-	borderBottomRightRadius?:number;
-	borderBottomWidth?:number;
-	borderColor?:color;
-	borderLeftColor?:color;
-	borderLeftWidth?:number;
-	borderRadius?:number;
-	borderRightColor?:color;
-	borderRightWidth?:number;
-	borderStyle?:"solid"|"dotted"|"dashed";
-	borderTopColor?:color;
-	borderTopLeftRadius?:number;
-	borderTopRightRadius?:number;
-	borderTopWidth?:number;
-	borderWidth?:number;
-	opacity?:number;
-	overflow?:"visible"|"hidden";
-}
-
-export interface TextStyles {
-	//tt:string|number;
-}
-
-/*
-//AllStyles utile si l'on veut restreindre les types et attributs possibles à injecter dans class StyleSheet:
-//Dans ce cas, on aurait définit:
-interface AllStyles extends ViewStyles, TextStyles {
-}
-class StyleSheet {
-	static create(obj:{[key: string]:AllStyles}):any;
-	static hairlineWidth:number;
-}
-*/
-
-//PROPERTIES:
-//Définition propriétés <View/>:
-export interface ViewPropsAndroid {
-
-}
-export interface ViewProps {
-	accessibilityLabel?:string;
-	accessible?:boolean;
-	onAccessibilityTap?:() => void;
-	onLayout?:NativeEventHandler<EventLayout>;
-	onMagicTap?: () => void;
-	//cf. https://facebook.github.io/react-native/docs/gesture-responder-system.html:
-	onMoveShouldSetResponder?: () => void;
-	onMoveShouldSetResponderCapture?: () => void;
-
-	style?:ViewStyles;
-}
-
-/*
+//Pour plus de détails d.ts JSX: https://github.com/Microsoft/TypeScript/wiki/JSX
 declare namespace JSX {
+	/*
 	//Les éléments intrinsèques permettent de définir rapidement des
 	//éléments dom, vues... n'étant pas des composants
 	//(par exemple, balises HTML pour react.js (cf. react.d.ts))
@@ -161,7 +189,9 @@ declare namespace JSX {
 	interface IntrinsicElements {
 		toto: { bar?: boolean }
 	}
+	*/
 
+	/*
 	//JSX.ElementAttributesProperty permet de spécifier le nom de l'objet
 	//qui sera utilisé comme référence pour vérifier le type des propriétés d'un
 	//composant [ElementAttributesProperty ne doit renseigner qu'un
@@ -170,36 +200,32 @@ declare namespace JSX {
 	interface ElementAttributesProperty {
 		props: Object;
 	}
+	*/
 }
-*/
 
+
+
+//On redéfinit ici les classes et fonctions pour permettre des imports unitaires
+//(du style import {Component} from... sans React.Component),
+//Il est inutile de redéfinir des interfaces (qui ne sont pas pris en compte en javascript: ils ne
+//servent qu'au typage, l'utilsateur devra faire React.EventLayout pour les utiliser via React importé en default):
 export class Component<P, S> extends React.Component<P,S> {
 }
-//COMPONENTS:
-export class Text extends Component<any, any> {
+export class Text extends __React.Text {
 }
-/*
-//class View extends Component<ViewProps, any> équivalent à:
-class View extends Component<any, any> {
-	props: {
-		testprop:string
-	}
-	render():JSX.Element;
+export class View extends __React.View {
 }
-*/
-export class View extends Component<ViewProps, any> {
+export class StyleSheet extends __React.StyleSheet {
 }
-export class StyleSheet {
-	static create(obj:{[key: string]:any}):any;
-
-	static hairlineWidth:number;
-}
-
-//Autres variables utiles:
 export var PropTypes:React.ReactPropTypes;
 
 
-export default React;
+//On exporte par défault le namespace __React, ce dernier contiendra au final à la fois les définitions react-native mais aussi react
+//(cf. plus haut avec explication namespace et main.d.ts):
+export default __React;
+
+
+
 
 
 /*
---
+++ test/test.tsx
@@ -0,0 +1,42 @@
+/*
+	Typescript target version: ES6 (to allow Babel and Webpack to read and bundle js transpiled file)
+ */
+//import * as React from "react-native";
+import React, {View, Text, PropTypes, StyleSheet, Component} from "react-native";
+
+
+let styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		transform: [{rotate: '50deg'}],
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: "black"
+	}
+});
+
+
+class App extends Component<any, any> {
+	static propTypes = {
+		testPropTypes: PropTypes.string
+	};
+
+	public _onLayout(event:React.EventLayout):void {
+		this.setState({
+			changeWidth: event.nativeEvent.layout.width - 6,
+			changeHeight: event.nativeEvent.layout.height - 6
+		});
+	}
+
+	render() {
+		return (
+			<View style={styles.container} onLayout={this._onLayout} onAccessibilityTap={() => {}}>
+				<Text>
+					Test Typescript React-Native Ayoub
+					</Text>
+			</View>
+		)
+	}
+}
+
+export default App;
